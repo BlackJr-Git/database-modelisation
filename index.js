40cc160f01ea011cff36f00bcd4bafb50c78e51d
Asosa <junior.asosa@iMac-10.local>
2024-03-26 15:50:30 +0100

    feat : set a prisma schema for database manipulation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import cors from "cors";
-import express, { json } from "express";
+// import cors from "cors";
+const cors = require("cors");
+const express = require("express");
+const { json }  = require('express')
+// import { json } from "express";
 // import {
 //   authBaseURI,
 //   rolesBaseURI,
@@ -17,17 +20,13 @@ import express, { json } from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: [
-    `http://localhost:${PORT}`,"*"
-  ],
+  origin: [`http://localhost:${PORT}`, "*"],
 };
 // dotenv.config();
-
 
 // Config
 app.use(json());
 app.use(cors());
-
 
 // Routes
 // app.use(authBaseURI, authRouter);
