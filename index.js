1bab7edceb9877aaa0d8af5df3ee4a5cf2ca54ab
Junior Asosa <juniorassosa@gmail.com>
2024-03-26 13:57:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import cors from "cors";
+import express, { json } from "express";
+// import {
+//   authBaseURI,
+//   rolesBaseURI,
+//   usersBaseURI,
+//   productBaseURI,
+// } from "./config/paths.js";
+
+// import {
+//   authRouter,
+//   roleRouter,
+//   userRouter,
+//   productRouter,
+// } from "./routes/index.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const corsOptions = {
+  origin: [
+    `http://localhost:${PORT}`,"*"
+  ],
+};
+// dotenv.config();
+
+
+// Config
+app.use(json());
+app.use(cors());
+
+
+// Routes
+// app.use(authBaseURI, authRouter);
+// app.use(rolesBaseURI, roleRouter);
+// app.use(usersBaseURI, userRouter);
+// app.use(productBaseURI, productRouter);
+
+app.listen(PORT, () => {
+  console.log(`The server listens on http://localhost:${PORT}`);
+});
